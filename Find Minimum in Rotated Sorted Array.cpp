e99197bb826515b6805f29af95c01bc131c05140
YuxuanHe <yuxuanhe2016@u.northwestern.edu>
2015-10-23 18:44:12 -0500

    location of function

--- Find Minimum in Rotated Sorted Array.cpp
+++ Find Minimum in Rotated Sorted Array.cpp
@@ -1,17 +1,18 @@
 class Solution {
 public:
-    int findMin(vector<int>& nums) {
-      return binary_search(0, nums.szie()-1, nums);
+  int binary_search(int start, int end, vector<int> &nums) {
+    int mid = (start + end)/2;
+    if(start == end)
+        return nums[mid];
+    if(nums[end] > nums[mid]) {
+        return binary_search(start, mid, nums);
     }
-    int binary_search(int start, int end, vector<int> &nums) {
-      if(start > end)
-        return nums[end];
-      int mid = (start + end)/2;
-      if(nums[end] > nums[mid]) {
-        return binary_search(start, mid-1, nums);
-      }
-      else{
+    else{
         return binary_search(mid+1, end, nums);
-      }
     }
+  }
+
+  int findMin(vector<int>& nums) {
+    return binary_search(0, nums.size()-1, nums);
+  }
 };
